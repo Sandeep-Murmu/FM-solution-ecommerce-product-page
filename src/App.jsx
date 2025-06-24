dbbd441b00d99a3692e5f12786326c0a43983f43
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-06-24 10:46:29 +0530

    update product detail information

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import { Col, Container, Row } from "react-bootstrap";
 import ProductDetail from "./components/main/ProductDetail";
 import ProductImageLayout from "./components/main/ProductImageLayout";
 
+const product = {
+  productTag: "sneaker company",
+  productName: "Fall limited Edition Sneakers",
+  productDescription:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+  productPrice: 250,
+  productNetPrice: 125,
+  productDiscount: 50,
+};
+
 function App() {
   return (
     <div className="main">
@@ -10,8 +20,8 @@ function App() {
           <Col>
             <ProductImageLayout />
           </Col>
-          <Col>
-            <ProductDetail />
+          <Col className="d-flex flex-column justify-content-center">
+            <ProductDetail product={product} />
           </Col>
         </Row>
       </Container>
--- src/components/main/ProductDetail.jsx
+++ src/components/main/ProductDetail.jsx
@@ -1,9 +1,31 @@
-function ProductDetail() {
-    return (
-        <div className="product-detail">
-            <h1>Produt details</h1>
+import { Container } from "react-bootstrap";
+
+function ProductDetail({ product }) {
+  const {
+    productName,
+    productTag,
+    productDescription,
+    productPrice,
+    productNetPrice,
+    productDiscount,
+  } = product;
+
+  return (
+    <div className="product-detail">
+      <Container fluid="true" className="mt-5 mb-4 product-detail__container">
+        <h4 className="product-detail__tag">{productTag}</h4>
+        <h1 className="product-detail__name">{productName}</h1>
+        <p className="product-detail__para">{productDescription}</p>
+        <div className="display-price1 d-flex align-items-baseline ">
+          <h2 className="product-detail__netprice">
+            ${productNetPrice.toFixed(2)}
+          </h2>
+          <h3 className="product-detail__discount">{productDiscount}%</h3>
         </div>
-    )
+        <h5 className="product-detail__price">${productPrice.toFixed(2)}</h5>
+      </Container>
+    </div>
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
